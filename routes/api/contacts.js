701b4b74a2ce20b3b740a663aa2f30af544e93a7
PokoenkoS <pokoenkosveta@gmail.com>
2023-05-22 10:59:35 +0300

    add ctrl

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,77 +1,22 @@
 const express = require('express')
-const Joi = require("joi")
 
-const contacts = require("../../models/contacts")
-const addShema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.number().required(),
-})
+const ctrl = require("../../controllers/contacts")
+
+const {validateBody} = require("../../middlewares")
+
+const schemas = require("../../schemas/contacts")
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result)
-})
+router.get('/', ctrl.getAll);
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const {id} =req.params;
-    const result = await contacts.getContactById(id)
-    if(!result) {
-      res.status(404) .json({ message: 'Not found' })
-     
-    }
-    res.json(result)
-  } catch (error) {
-    next(error)
-  }
+router.get('/:contactId', ctrl.getById);
+
+router.post('/', validateBody(schemas.addSchema), ctrl.add)
+
+router.delete('/:contactId', ctrl.deleteById);
+
+router.put('/:contactId', validateBody(schemas.addSchema), ctrl.updateById);
  
-})
 
-router.post('/', async (req, res, next) => {
-try {
-  const {error} =addShema.validate(req.body);
-  if(error){
-    res.status(404) .json({ message: "missing required name field"})
-  }
-  const result = await contacts.addContact(req.body);
-  res.status(201).json(result);
-} catch (error) {
-  next(error)
-}
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const {id} =req.params;
-    const result = await contacts.removeContact(id)
-    if(!result) {
-      res.status(404) .json({ message: 'Not found' })
-     
-    }
-    res.status(200).json({ message: "contact deleted"})
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const {error} =addShema.validate(req.body);
-    if(error){
-      res.status(400) .json({ message: "missing fields"})
-    }
-    const{contactId}=req.params;
-    const result = await contacts.updateContact(contactId, req.body);
-    if(!result){
-      res.status(404) .json({ message: 'Not found' })
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error)
-  }
-})
-
-module.exports = router
+module.exports = router;
