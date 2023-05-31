3077665e95de83be000fd6f23ed3af279c3896ce
PokoenkoS <pokoenkosveta@gmail.com>
2023-05-31 16:05:00 +0300

    add

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 const {HttpError} = require("../../helpers");
-=======
-const {HttpError} = require("../helpers");
->>>>>>> d21917d2224ecfab74a7eb3b194385fc4e132463
+
 
 const validateBody = schema => {
     const func = (req, res, next)=> {
